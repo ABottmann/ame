a3cc90e4dfb8abe2ecf1f2d79ffd18a41e176cff
forum <forum@cyberware.com.br>
2021-08-01 17:29:13 -0300

    Logging stuff...

--- src/main/java/com/ebanx/ame/controller/AccountController.java
+++ src/main/java/com/ebanx/ame/controller/AccountController.java
@@ -14,34 +14,46 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
 @RestController
 public class AccountController
 {
+    private static final Logger logger = Logger.getLogger(AccountController.class.getName());
+
     @Autowired
     private AccountService accountService;
     private ObjectMapper mapper = new ObjectMapper();
 
     @PostMapping("/reset")
     public ResponseEntity<String> reset() {
+        logger.info("Reset requested");
         accountService.reset();
         return  ResponseEntity.ok("OK");
     }
 
     @GetMapping("/balance")
     public ResponseEntity<Integer> getBalance(@RequestParam("account_id") String account_id){
+        logger.info("Balance requested for account " + account_id);
+
         Account account = accountService.getAccountById(account_id);
         if (account == null)
             return new ResponseEntity<>(0, HttpStatus.NOT_FOUND);
+
+        logger.info(" Balance for account " + account_id + " sent");
 
         return new ResponseEntity<>(account.getBalance(), HttpStatus.OK);
     }
 
     @PostMapping("/event")
     public ResponseEntity<String> event(@RequestBody Event event) throws JsonProcessingException {
+        String eventType = event.getType();
+        logger.info("Event " + eventType + " requested ");
+
         String destinationAccountNumber = event.getDestination();
         Account destination = accountService.getAccountById(destinationAccountNumber);
         Account origin = accountService.getAccountById(event.getOrigin());
-        String eventType = event.getType();
+
         int amount = event.getAmount();
 
         if (event.getType().equals(EventType.DEPOSIT.getTypeCode()))
@@ -53,23 +65,29 @@ public class AccountController
         if (event.getType().equals(EventType.TRANSFER.getTypeCode()))
             return Transfer(destinationAccountNumber, origin, destination, amount);
 
+        logger.warning("Event " + eventType + " not valid ");
         return new ResponseEntity<>("0", HttpStatus.NOT_FOUND);
     }
 
 
     private ResponseEntity<String> Deposit(String destinationAccountNumber, Account destination, int amount) throws JsonProcessingException {
-        if (destination == null)
+        if (destination == null) {
             destination = accountService.CreateAccount(destinationAccountNumber, amount);
-        else
+            logger.info(" Destination account created with credit of " + amount);
+        }
+        else {
             accountService.Transaction(destination, amount);
+        }
 
         return new ResponseEntity<>(mapper.writeValueAsString(new Destination(destination)), HttpStatus.CREATED);
     }
 
 
     private ResponseEntity<String> Withdraw(Account origin, int amount) throws JsonProcessingException {
-        if (origin == null)
+        if (origin == null) {
+            logger.warning(" Withdraw was not realized");
             return new ResponseEntity<>("0", HttpStatus.NOT_FOUND);
+        }
 
         accountService.Transaction(origin, amount * -1);
 
@@ -78,8 +96,10 @@ public class AccountController
 
 
     private ResponseEntity<String> Transfer(String destinationAccountNumber, Account origin, Account destination, int amount) throws JsonProcessingException {
-        if (origin == null)
+        if (origin == null) {
+            logger.warning(" Transfer was not realized");
             return new ResponseEntity<>("0", HttpStatus.NOT_FOUND);
+        }
 
         if (destination == null)
             destination = accountService.CreateAccount(destinationAccountNumber, 0);
--- src/main/java/com/ebanx/ame/service/AccountService.java
+++ src/main/java/com/ebanx/ame/service/AccountService.java
@@ -5,52 +5,65 @@ import com.ebanx.ame.model.Account;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @Service
 public class AccountService {
 
+    private static final Logger logger = Logger.getLogger(AccountService.class.getName());
+
     private LinkedList<Account> accounts;
     private Account account;
 
-    public Account CreateAccount(String idAccount, int amount ) {
+    public Account CreateAccount(String id, int amount ) {
 
         if (accounts == null)
             accounts = new LinkedList<>();
 
-        accounts.add(new Account(idAccount, amount));
+        accounts.add(new Account(id, amount));
+        logger.info(" Account " + id + " has been created");
 
-        return getAccountById(idAccount);
-
+        return getAccountById(id);
     }
 
 
     public void reset(){
         if (accounts != null)
             accounts.clear();
+
+        logger.info(" Reseted");
     }
 
 
     public void Transaction(Account account, int amount){
         amount = account.getBalance() + amount;
         account.setBalance(amount);
+        logger.info(" Transaction for account " + account.getId() + " has been realized");
     }
 
     public Account getAccountById(String account_id){
         this.account = null;
 
-        try {
-             this.account = accounts.stream().filter(t -> account_id.equals(t.getId())).collect(Collectors.toList()).get(0);
-        }
-        catch(Exception e){
+        if (account_id != null) {
+            logger.info(" Looking for account " + account_id);
 
+            try {
+                this.account = accounts.stream().filter(t -> account_id.equals(t.getId())).collect(Collectors.toList()).get(0);
+            } catch (Exception e) {
+                logger.warning(" Account " + account_id + " not found ");
+
+            }
         }
+
         return this.account;
     }
 
     public Transfer Transfer(Account origin, Account destination, int amount) {
         this.Transaction(origin, amount * -1);
         this.Transaction(destination, amount );
+
+        logger.info(" Transfer from account " + origin.getId() + " to account " + destination.getId() + " realized");
 
         return new Transfer(origin, destination);
     }
